6baf290aaf62a16a9e89c1a6e50a549d126fda15
gabri <gabriellondero40@gmail.com>
2023-10-12 07:11:51 -0300

    completando los imput dando estilos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  console.log(email);
+
   return (
     <div>
-      <h1>Login</h1>
+      <form>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+          name="email"
+          type="email"
+          value={email}
+        />
+        <button>Send</button>
+      </form>
     </div>
   );
 };
