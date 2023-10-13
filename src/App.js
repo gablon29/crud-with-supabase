76ef05221981118aa629720b6c1525904449f13c
gabri <gabriellondero40@gmail.com>
2023-10-13 18:10:45 -0300

    desarrollando las restricciones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+import { client } from "./supabase/client";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    client.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        navigate("/login");
+      } else {
+        navigate("/");
+      }
+    });
+  }, [navigate]);
+
   return (
     <div className="App">
       <Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { client } from "../supabase/client";
 
 export default function Home() {
   return (
     <div>
       <h1>Hola mundo</h1>
+      <button onClick={() => client.auth.signOut()}>Logout</button>
     </div>
   );
 }
